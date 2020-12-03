f90dd5b99f13c50d7d25066de8b531038dd6c921
Mohsen Barzegar <mohsennbarzegar@gmail.com>
2020-12-03 13:02:42 +0330

    new

--- static/panjere.js
+++ static/panjere.js
@@ -1,17 +1,23 @@
-const URL = 'https://panjere-sehraramiz.fandogh.cloud/api/random/5/'
-const myHeaders = new Headers();
-myHeaders.append('Accept', 'application/json');
+function showPanjere(URL) {
 
-fetch(URL, {
-    method: "GET",
-    headers: myHeaders,
-})
-.then(res => res.json())
-.then((links) => {
-    var out = "";
-    for(var link of links) {
-        out += '<li> <i class="fa fa-external-link" aria-hidden="true"></i> <a href="' + link.text + '" target="_blank" style="target-new: tab;" >' +
-        link.title + '</a></li>';
+  const myHeaders = new Headers();
+  myHeaders.append('Accept', 'application/json');
+
+  fetch(URL, {
+      method: "GET",
+      headers: myHeaders,
+  })
+  .then(res => res.json())
+  .then((links) => {
+      var out = "";
+      for(var link of links) {
+          out += '<li> <i class="fa fa-external-link" aria-hidden="true"></i> <a href="' + link.text + '" target="_blank" style="target-new: tab;" >' +
+          link.title + '</a></li>';
+  }
+  document.getElementById("panjere").style.direction = "rtl";
+  document.getElementById("panjere").style.listStyleType = "none";
+  document.getElementById("panjere").innerHTML = out;
+
+  })
+
 }
-document.getElementById("panjere").innerHTML = out;
-})
