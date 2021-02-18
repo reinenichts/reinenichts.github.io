1ed78674f0255b2f2c74d9b050883f81d714d83c
Mohsen Barzegar <mohsennbarzegar@gmail.com>
2021-02-18 22:38:47 +0330

    new

--- static/panjere.js
+++ static/panjere.js
@@ -1,20 +1,19 @@
 function showPanjere(URL) {
 
     var xmlhttp = new XMLHttpRequest();
-    var url = "https://panjere-sehraramiz.fandogh.cloud/api/random/5/";
 
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           console.log(JSON.parse(xmlhttp.response));
         var myArr = JSON.parse(xmlhttp.response);
-        myFunction(myArr);
+        showRandomLinks(myArr);
       }
     };
-    xmlhttp.open("GET", url, true);
+    xmlhttp.open("GET", URL, true);
     xmlhttp.setRequestHeader('Accept', 'application/json');
     xmlhttp.send();
 
-    function myFunction(links) {
+    function showRandomLinks(links) {
         var out = "";
         for(var i = 0; i < links.length; i++) {
             out += '<li> <i class="fa fa-external-link" aria-hidden="true"></i> <a href="' + links[i].text + '" target="_blank" style="target-new: tab;" >' +
